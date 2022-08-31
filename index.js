153f178052f16025cdceca044c112d0e2272f80e
rodicach <rodicachavez@pursuit.org>
2022-08-30 20:51:32 -0400

    Weather App 1 Day 1

---
+++ index.js
@@ -0,0 +1,16 @@
+// const body = document.querySelector("body")
+
+// const h1 = document.createElement("h1")
+// const form = document.createElement("form")
+// const label = document.createElement("button")
+// const input = document.createElement("input")
+
+// h1.append(form)
+// form.append(label)
+// label.append(input)
+// label.after(button)
+
+
+// h1.textContent - "Get Weather"
+
+
