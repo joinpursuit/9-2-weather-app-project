5b4f3c556d22a7c8363241c24f459c060fb3507f
Isaac Guerrero <isaacguerrero@pursuit.org>
2022-08-31 16:51:32 -0400

    Update index.html main.js styles.css

---
+++ main.js
@@ -0,0 +1,8 @@
+/* Add event listener to search bar */
+const form = document.querySelector("form")
+form.addEventListener("submit", (event) => {
+    const main = document.querySelector("main");
+
+    // Create 3 sections below main section to show information for the next few days
+    // add link to the search to the sidebar (ul -> li)
+}) 
