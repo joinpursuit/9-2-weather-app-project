015dc3d4c6b5aa0bacd42dcbb321c28d62060d72
Wwright91 <wisdomwright-martin@pursuit.org>
2022-08-30 23:17:02 -0400

    html almost done, added a touch of css

---
+++ index.js
@@ -0,0 +1,15 @@
+// const h1 = document.querySelector("h1");
+
+// const form = document.createElement("form");
+// const label = document.createElement("label");
+// const button = document.createElement("button");
+// const input = document.createElement("input");
+
+// h1.append(form);
+// form.append(label);
+// label.append(input);
+// label.after(button);
+
+// button.textContent = "Get Weather";
+
+
