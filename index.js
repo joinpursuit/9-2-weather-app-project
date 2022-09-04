781462239c7c14a4c87590b029020aa927ed1cee
Wwright91 <wisdomwright-martin@pursuit.org>
2022-09-04 16:54:39 -0400

    temp converter working

--- index.js
+++ index.js
@@ -2,13 +2,15 @@ const form = document.querySelector("form");
 const article = document.querySelector("article");
 const placeholder = document.querySelector("p.placeholder");
 const previous = document.querySelector("p.previous");
-const header = document.querySelector("header")
+const header = document.querySelector("header");
+const main = document.querySelector("main");
 
 form.addEventListener("submit", (event) => {
   event.preventDefault();
   placeholder.style.setProperty("display", "none");
   previous.style.setProperty("display", "none");
   const userInput = event.target.userInput.value;
+  // console.log(userInput)
   const baseUrl = `https://wttr.in/${userInput}?format=j1`;
   form.reset();
   // console.log(baseUrl)
@@ -17,72 +19,7 @@ form.addEventListener("submit", (event) => {
   fetch(baseUrl)
     .then((response) => response.json())
     .then((weather) => {
-      //set image. need to alternate image depending on data. need to set alt
-      const img = document.createElement("img");
-      img.setAttribute("src", "/assets/icons8-night.gif");
-      // article.append(img);
-
-      //this gives the city of the search
-      const city = weather.nearest_area[0].areaName[0].value;
-      const h2 = document.createElement("h2");
-      h2.textContent = city;
-      // article.append(h2);
-
-      //this will display the nearest area if the city doesn't match the users input
-      const area = document.createElement("p");
-      if (city !== userInput) {
-        area.innerHTML = "<strong>Nearest Area: </strong>" + city;
-      } else {
-        area.innerHTML = "<strong>Area: </strong>" + city;
-      }
-      // article.append(area);
-      // console.log(city);
-
-      //this gives the country of the search
-      const country = weather.nearest_area[0].country[0].value;
-      const pcountry = document.createElement("span");
-      pcountry.innerHTML = "<strong>Country: </strong>" + country;
-
-      //this gives the region of the search
-      const region = weather.nearest_area[0].region[0].value;
-      const pregion = document.createElement("p");
-      pregion.innerHTML = "<strong>Region: </strong>" + region;
-
-      //this gives the currently feels like temp of the search in F
-      const feelsLikeF = weather.current_condition[0].FeelsLikeF;
-      const pfeels = document.createElement("p");
-      pfeels.innerHTML =
-        "<strong>Currently: </strong>" + "Feels Like " + feelsLikeF + "°F";
-
-      //chance of sun
-      const chanceOfSunshsine = weather.weather[0].hourly[0].chanceofsunshine;
-      const sunny = document.createElement("p");
-      sunny.innerHTML =
-        "<strong>Chance of Sunshine: </strong>" + chanceOfSunshsine;
-      // console.log(chanceOfSunshsine);
-
-      //chance of rain
-      const chanceOfRain = weather.weather[0].hourly[0].chanceofrain;
-      const rainy = document.createElement("p");
-      rainy.innerHTML = "<strong>Chance of Rain: </strong>" + chanceOfRain;
-
-      //chance of snow
-      const chanceOfSnow = weather.weather[0].hourly[0].chanceofsnow;
-      const snowy = document.createElement("p");
-      snowy.innerHTML = "<strong>Chance of Snow: </strong>" + chanceOfSnow;
-      // console.log(chanceOfRain);
-      // console.log(chanceOfSnow);
-      article.append(
-        img,
-        h2,
-        area,
-        pregion,
-        pcountry,
-        pfeels,
-        sunny,
-        rainy,
-        snowy
-      );
+      pullWeather(weather);
 
       // See detailed information for the current day and the next two days below the `main` element.
       const artToday = document.querySelector("article.today");
@@ -174,28 +111,126 @@ form.addEventListener("submit", (event) => {
         pday_afterMax,
         pday_afterMin
       );
-
-      //previous section
-      const ul = document.querySelector("ul");
-      const li = document.createElement("li");
-      ul.append(li);
-      //should reload to same page
-      const a = document.createElement("a");
-      a.setAttribute("href", baseUrl);
-      // a.setAttribute("target", "_blank");
-      a.innerHTML = city;
-      const aTemp = document.createElement("p");
-      aTemp.textContent = " - " + feelsLikeF + "°F"
-        li.after(a, aTemp);
-      // li.textContent = city;
-
-//style article
-      // article.style.border.
-
-
-
     })
     .catch((error) => {
       console.log(error);
     });
+});
+
+function pullWeather(weather) {
+  //this gives the city of the search
+  const city = weather.nearest_area[0].areaName[0].value;
+  const h2 = document.createElement("h2");
+  h2.textContent = city;
+
+  //this will display the nearest area if the city doesn't match the users input
+  const area = document.createElement("p");
+  if (city !== userInput) {
+    area.innerHTML = "<strong>Nearest Area: </strong>" + city;
+  } else {
+    area.innerHTML = "<strong>Area: </strong>" + city;
+  }
+
+  //this gives the country of the search
+  const country = weather.nearest_area[0].country[0].value;
+  const pcountry = document.createElement("p");
+  pcountry.innerHTML = "<strong>Country: </strong>" + country;
+
+  //this gives the region of the search
+  const region = weather.nearest_area[0].region[0].value;
+  const pregion = document.createElement("p");
+  pregion.innerHTML = "<strong>Region: </strong>" + region;
+
+  //this gives the currently feels like temp of the search in F
+  const feelsLikeF = weather.current_condition[0].FeelsLikeF;
+  const pfeels = document.createElement("p");
+  pfeels.innerHTML =
+    "<strong>Currently: </strong>" + "Feels Like " + feelsLikeF + "°F";
+
+  //chance of sun
+  const chanceOfSunshsine = weather.weather[0].hourly[0].chanceofsunshine;
+  const sunny = document.createElement("p");
+  sunny.innerHTML =
+    "<strong>Chance of Sunshine: </strong>" + chanceOfSunshsine + "%";
+  // console.log(chanceOfSunshsine);
+
+  //chance of rain
+  const chanceOfRain = weather.weather[0].hourly[0].chanceofrain;
+  const rainy = document.createElement("p");
+  rainy.innerHTML = "<strong>Chance of Rain: </strong>" + chanceOfRain + "%";
+
+  //chance of snow
+  const chanceOfSnow = weather.weather[0].hourly[0].chanceofsnow;
+  const snowy = document.createElement("p");
+  snowy.innerHTML = "<strong>Chance of Snow: </strong>" + chanceOfSnow + "%";
+  // console.log(chanceOfRain);
+  // console.log(chanceOfSnow);
+
+  //the weather icon should change depending on this info
+  //  - if there is more than a 50% chance of sunshine, show the `summer` icon with `alt` text `sun`
+  //  - if there is more than a 50% chance of rain, show the `torrential-rain` icon with `alt` text `rain`
+  //  - if there is more than a 50% chance of snow, show the `light-snow` icon with `alt` text `snow`
+
+  //test is not changing from sun to rain or snow
+  const img = document.createElement("img");
+
+  if (Number(chanceOfSunshsine) > 50) {
+    img.src = "./assets/icons8-summer.gif";
+    img.alt = "sun";
+  }
+  if (Number(chanceOfRain) > 50) {
+    img.src = "./assets/icons8-torrential-rain.gif";
+    img.alt = "rain";
+  }
+  if (Number(chanceOfSnow) > 50) {
+    img.src =  "./assets/icons8-light-snow.gif";
+    img.alt = "snow";
+  }
+
+  //       console.log(chanceOfSunshsine)
+
+  //       console.log(typeof chanceOfSunshsine)
+  // console.log(Number(chanceOfSunshsine))
+
+  //previous section
+  const ul = document.querySelector("ul");
+  const li = document.createElement("li");
+  ul.append(li);
+  //should reload to same page
+  const a = document.createElement("a");
+  a.setAttribute("href", "");
+  // a.setAttribute("target", "_blank");
+  a.innerHTML = city;
+  const aTemp = document.createElement("p");
+  aTemp.textContent = " - " + feelsLikeF + "°F";
+  li.after(a, aTemp);
+  // li.textContent = city;
+
+  //style article
+  // article.style.border.
+
+  article.append(img, h2, area, pregion, pcountry, pfeels, sunny, rainy, snowy);
+}
+
+//temp converter working
+const form2 = document.querySelector(".widget");
+form2.addEventListener("submit", (event) => {
+  event.preventDefault();
+  const valNum = event.target.conversion.value;
+  console.log(valNum);
+
+  const celsius = document.querySelector("#to-c");
+  const fahrenheit = document.querySelector("#to-f");
+  const h4 = document.querySelector("#temp-conv");
+
+      if (celsius.checked) {
+        const ctof = (valNum - 32) / 1.8
+        console.log(ctof)
+    h4.textContent = ctof.toFixed(2)
+  }
+      if (fahrenheit.checked) {
+        const ftoc = (valNum * 1.8) + 32
+        console.log(ftoc)
+        h4.textContent = ftoc.toFixed(2)
+      }
 });
